4c3223729ffd7a28c36d07c1c1cb881bdd67aac2
aiml096 <22cseaiml096.potnuruvamsikrishna@giet.edu>
2025-03-16 09:29:17 +0530

    api testing is working

---
+++ app.py
@@ -0,0 +1,22 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+model=pickle.load(open("boston_model.pkl","rb"))
+scaler=pickle.load(open("scaler.pkl","rb"))
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/predict_api",methods=["POST"])
+def predict_api():
+    data=request.json["data"]
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output=model.predict(new_data)
+    return jsonify(output[0])
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
